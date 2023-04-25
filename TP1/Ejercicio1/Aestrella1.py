df88ffafe9e78e3731461c3164435b701bd2af7d
99randon <brandonmamani1999@gmail.com>
2023-04-25 17:40:09 -0300

    E1 ACTUALIZADO

--- TP1/Ejercicio1/Aestrella1.py
+++ TP1/Ejercicio1/Aestrella1.py
@@ -1,90 +1,142 @@
 import matplotlib.pyplot as plt
-from Grafico1 import Grafico
+from Grafico import Grafico
+import copy
+from math import dist
 
-# Funcion que devuelve la distancia entre dos puntos para ser utilizada como heuristica
-def heuristica (x,y,j,k):
-    return (abs(x-j)**2+abs(y-k)**2)**0.5 * 100
+class Aestrella():
+    def __init__(self,filas,columnas):
 
-# Funcion que calcula los nodos donde se encuentran estantes
-def CalcObstaculos(Cantfilas,Cantcolumnas):
+        # Cantidad de filas y columnas de estantes
+        self.Cantfilas = filas
+        self.Cantcolumnas = columnas
+        self.Obstaculos= self.CalcObstaculos()
 
-    Obstaculos = []
-    vecfilas = []
-    veccolumnas = []
+    def calcularcamino(self,inicio,final):
 
-    n=2
-    for i in range(Cantfilas):
-        vecfilas.append(n)
-        vecfilas.append(n+1)
-        vecfilas.append(n+2)
-        vecfilas.append(n+3)
-        n+=6
+        nodoinicial = list(self.Obstaculos[inicio-1])
+        nodofinal = list(self.Obstaculos[final-1])
 
-    n=2
-    for i in range(Cantcolumnas):
-        veccolumnas.append(n)
-        veccolumnas.append(n+1)
-        n+=4
+        nodofinalp = nodofinal.copy()
 
-    for j in veccolumnas:
-        for i in vecfilas:
-            Obstaculos.append((i-1,j-1))
-    return Obstaculos
+        nodosabiertos = []
+        nodosvisitados = []
 
-# Funcion que calcula el camino mas corto entre dos puntos utilizando el algoritmo de busqueda A*
-def Aestrella():
+        if ((nodofinalp[0],nodofinalp[1])) in self.Obstaculos:
+            nodofinalp[1] += 1
+            if ((nodofinalp[0],nodofinalp[1])) in self.Obstaculos:
+                nodofinalp[1] -= 2
 
-    # Cantidad de filas y columnas de estantes
-    Cantfilas = 8
-    Cantcolumnas= 9
+        if ((nodoinicial[0],nodoinicial[1])) in self.Obstaculos:
+            nodoinicial[1] += 1
+            if ((nodoinicial[0],nodoinicial[1])) in self.Obstaculos:
+                nodoinicial[1] -= 2
+        r=0
+        nodoactual = nodoinicial
+        nodosvisitados.append(((nodoinicial[0]),(nodoinicial[1]),r))
 
-    # Nodo final y nodo inicial
-    nodofinal = [15,14]
-    nodoinicial = [0,0]
+        if nodoactual == nodofinalp:
+            return 0, [nodoinicial, nodofinalp], nodofinalp
+        
+        while True:
 
-    # Algoritmo de busqueda A*
-    nodofinalp = nodofinal.copy()
-    nodoactual = nodoinicial
+            if nodoactual == nodofinalp:
+                break
 
-    nodosabiertos = {}
-    nodosvisitados = []
+            r+=1
+            iactual = nodoactual[0]
+            jactual = nodoactual[1]
 
-    Obstaculos= CalcObstaculos(Cantfilas,Cantcolumnas)
+            for i in [-1, 0, 1]:
+                for j in [-1, 0, 1]:
+                    inuevo = iactual + i
+                    jnuevo = jactual + j
+                    if ((inuevo, jnuevo) not in [(sublst[0], sublst[1]) for sublst in nodosabiertos]) and ((inuevo, jnuevo) not in self.Obstaculos) and ((inuevo, jnuevo) not in [(sublst[0], sublst[1]) for sublst in nodosvisitados]) and (inuevo >= 0) and (jnuevo >= 0) and (inuevo <= self.Cantfilas * 6) and (jnuevo <= self.Cantcolumnas * 4) and abs(i) != abs(j):
+                        f = self.heuristica(nodofinalp[0], nodofinalp[1], inuevo, jnuevo) + r
+                        nodosabiertos.append((inuevo, jnuevo,f , r, (iactual, jactual)))
+            nodosabiertos = sorted(nodosabiertos, key=lambda x: x[2])
+            nodoactual[0],nodoactual[1], *_ = nodosabiertos[0]
+            nodosvisitados.append(((nodosabiertos[0][0]),(nodosabiertos[0][1]),(nodosabiertos[0][3]),(nodosabiertos[0][4])))
+            nodosabiertos.pop(0)            
 
-    if ((nodofinal[0],nodofinal[1])) in Obstaculos:
-            nodofinal[1] += 1
-            if ((nodofinal[0],nodofinal[1])) in Obstaculos:
-                nodofinal[1] -= 2
+        listafinal=[]
+        while True:
 
-    nodosabiertos[str(nodoactual[0])+","+str(nodoactual[1])] = heuristica(nodofinal[0],nodofinal[1],0,0)
-    nodosvisitados.append(((nodoactual[0]),(nodoactual[1])))
-
-    if ((nodofinalp[0],nodofinalp[1])) not in Obstaculos:
-        print("El nodo final no es un estante")
-
-    while True:
-
-        iactual = int(nodoactual[0])
-        jactual = int(nodoactual[1])
-
-
-        for i in [-1,0,1]:
-            for j in [-1,0,1]:
-                iactual+i
-                jactual+j
-                if ((f"{str(iactual+i)},{str(jactual+j)}") not in nodosabiertos.keys()) and (((iactual+i),(jactual+j)) not in Obstaculos) and (((iactual+i),(jactual+j)) not in nodosvisitados) and (iactual+i >= 0) and (jactual+j >= 0) and (iactual+i <= Cantfilas*6) and (jactual+j <= Cantcolumnas*4) and i != j:
-                    nodosabiertos[(f"{str(iactual+i)},{str(jactual+j)}")] = 100 + heuristica(nodofinal[0],nodofinal[1],iactual+i,jactual+j)
-
-        del nodosabiertos[str(nodoactual[0])+","+str(nodoactual[1])]
-        nodosabiertos = dict(sorted(nodosabiertos.items(), key=lambda item: item[1]))
-        nodoactual = [int(x) for x in ((list(nodosabiertos.keys())[0]).split(","))]
-        nodosvisitados.append(((nodoactual[0]),(nodoactual[1])))
+            for i in range(len(nodosvisitados)-1):
+                if len(nodosvisitados)==2:
+                    listafinal.insert(0,(nodosvisitados[-1][0],nodosvisitados[-1][1]))
+                    listafinal.insert(0,(nodosvisitados[0][0],nodosvisitados[0][1]))
+                    print(listafinal)
+                    return len(listafinal),listafinal,nodofinalp
+                
+                while nodosvisitados[-i-1][3] != (nodosvisitados[-i-2][0],nodosvisitados[-i-2][1]):
+                    nodosvisitados.pop(-i-2)
+                
+                listafinal.insert(0,(nodosvisitados[-i-1][0],nodosvisitados[-i-1][1]))
+                
+                if i ==len(nodosvisitados)-2:
+                    listafinal.insert(0,(nodosvisitados[0][0],nodosvisitados[0][1]))
+                    print(listafinal)
+                    return len(listafinal),listafinal,nodofinalp
+        
+    def dibujar(self,nodosvisitados,nodofinalp):    
+        Grafico1=Grafico(self.Cantfilas*6,self.Cantcolumnas*4,self.Obstaculos)
+        Grafico1.dibujar_grafico(nodosvisitados,nodofinalp)
 
 
-        if nodoactual == nodofinal:
-            break
+    def GraficarCamino (self,lista,lista2=0):
+
+        nodosvisitados = []
+        finales = []
+
+        for i in range(len(lista)-1):
+            
+            nodosvisitados.append((self.calcularcamino(lista[i],lista[i+1]))[1])
+
+        Grafico2=Grafico(self.Cantfilas*6,self.Cantcolumnas*4,self.Obstaculos)
+
+        for x in lista:
+            finales.append(self.Obstaculos[x-1])
+
+        if lista2==0:
+            Grafico2.dibujar_grafico(nodosvisitados,finales)
+        else:
+            total_elements = 0
+            for sublist in  nodosvisitados:
+                total_elements += len(sublist)
+            return total_elements
+        
+
+    def heuristica (self,x,y,j,k):
+        return (abs(x-j)**2+abs(y-k)**2)**0.5 
+
+    # Funcion que calcula los nodos donde se encuentran estantes
+    def CalcObstaculos(self):
+
+        Obstaculos = []
+        vecfilas = []
+        veccolumnas = []
+
+        n=2
+        for i in range(self.Cantfilas):
+            vecfilas.append(n)
+            vecfilas.append(n+1)
+            vecfilas.append(n+2)
+            vecfilas.append(n+3)
+            n+=6
+
+        n=2
+        for i in range(self.Cantcolumnas):
+            veccolumnas.append(n)
+            veccolumnas.append(n+1)
+            n+=4
+
+        for j in veccolumnas:
+            for i in vecfilas:
+                Obstaculos.append((i-1,j-1))
+        return Obstaculos
     
-    Grafico1=Grafico(Cantfilas*6,Cantcolumnas*4,Obstaculos,nodosvisitados,nodofinalp)
-    Grafico1.dibujar_grafico()
-
-Aestrella()
+AEstrella1 = Aestrella(4,6)
+inicio=18
+final=135
+AEstrella1.calcularcamino(inicio,final)
+AEstrella1.GraficarCamino([inicio,final])
--- TP1/Ejercicio1/Grafico1.py
+++ TP1/Ejercicio1/Grafico1.py
@@ -1,33 +1,38 @@
 import tkinter as tk
 
 class Grafico:
-    def __init__(self, filas, columnas, obstaculos, camino,final):
+    def __init__(self, filas, columnas, obstaculos):
         self.filas = filas
         self.columnas = columnas
         self.obstaculos = obstaculos
+
+    def dibujar_grafico(self,camino,final):
         self.camino = camino
         self.final = final
+        print(self.camino)
+        print(self.final)
 
-    def dibujar_grafico(self):
-        # Crear matriz con ceros
         matriz = [[0 for j in range(self.columnas)] for i in range(self.filas)]
-
 
         # Pintar obstáculos
         for i, j in self.obstaculos:
-            matriz[i][j] = 3
-        # Pintar camino
-        for i, j in self.camino:
             matriz[i][j] = 2
+        # Pintar camino
+        r=4
+        for sublist in self.camino:
+            for subsublist in sublist:
+                i, j = subsublist[0], subsublist[1]
+                matriz[i][j] = r
+            r+=1
+        # Pintar final
+        for i, j in self.final:
+            matriz[i][j] = 3
 
         # Pintar resto en blanco
         for i in range(self.filas):
             for j in range(self.columnas):
                 if matriz[i][j] == 0:
                     matriz[i][j] = 1
-
-        matriz[self.final[0]][self.final[1]]=4
-
         # Crear ventana
         ventana = tk.Tk()
         ventana.title("Grafico")
@@ -42,13 +47,50 @@ class Grafico:
                 y1 = i * 30
                 x2 = x1 + 30
                 y2 = y1 + 30
-                color = "white"
+                if matriz[i][j] == 1:
+                    color = "white"
                 if matriz[i][j] == 2:
-                    color = "yellow"
-                elif matriz[i][j] == 3:
                     color = "black"
-                elif matriz[i][j] == 4:
+                elif matriz[i][j] == 3:
                     color = "red"
+                elif matriz[i][j] == 4:
+                    color = "yellow"
+                elif matriz[i][j] == 5:
+                    color = "blue"
+                elif matriz[i][j] == 6:
+                    color = "green"
+                elif matriz[i][j] == 7:
+                    color = "orange"
+                elif matriz[i][j] == 8:
+                    color = "purple"
+                elif matriz[i][j] == 9:
+                    color = "grey"
+                elif matriz[i][j] == 10:
+                    color = "brown"
+                elif matriz[i][j] == 11:
+                    color = "pink"
+                elif matriz[i][j] == 12:
+                    color = "cyan"
+                elif matriz[i][j] == 13:
+                    color = "magenta"
+                elif matriz[i][j] == 14:
+                    color = "gold"
+                elif matriz[i][j] == 15:
+                    color = "silver"
+                elif matriz[i][j] == 16:
+                    color = "dark green"
+                elif matriz[i][j] == 17:
+                    color = "dark blue"
+                elif matriz[i][j] == 18:
+                    color = "dark red"
+                elif matriz[i][j] == 19:
+                    color = "dark grey"
+                elif matriz[i][j] == 20:
+                    color = "dark cyan"
+                elif matriz[i][j] == 21:
+                    color = "dark magenta"
+                elif matriz[i][j] == 22:
+                    color = "dark yellow"
                 lienzo.create_rectangle(x1, y1, x2, y2, fill=color)
 
         # Ajustar tamaño del lienzo
